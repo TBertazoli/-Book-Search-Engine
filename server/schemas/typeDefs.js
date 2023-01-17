cc6eecfab708bdf892fd4c88bd61a7f5eee31f6d
TB <tatianabertazoli@gmail.com>
2023-01-17 16:57:01 -0600

    added login user from mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,43 +2,43 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
-    _id: ID
-    username: String
-    email: String
-    bookCount: Int
-    savedBooks: [Book]    
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int!
+    savedBooks: [Book!]    
   }
 
   type Book {
-    bookId: String
-    authors: [Author]    
-    description: String   
-    title: String  
-    image: String
-    link: String       
+    bookId: String!
+    authors: [Author!]    
+    description: String! 
+    title: String! 
+    image: String!
+    link: String!      
   } 
 
   type Author{
-    name: String
+    name: String!
   }
 
   type Auth {
     token: ID!
-    user: User
+    user: User!
   }
 
   type Query {
-    me: User
-    users: [User]
-    user(username: String!): User
-    books(title: String): [Book]
-    book(bookId: String ): Book
+    me: User!
+    users: [User!]
+    user(username: String!): User!
+    books(title: String!): [Book!]
+    book(bookId: String! ): Book!
   }
 
   type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(author: String!, description: String!, title: String!, image: String!, link: String! ): Book
+    login(email: String!, password: String!): Auth!
+    addUser(username: String!, email: String!, password: String!): Auth!
+    saveBook(author: String!, description: String!, title: String!, image: String!, link: String! ): Book!
   }
 `;
 
--- server/server.js
+++ server/server.js
@@ -3,32 +3,40 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
-const {authMiddleware} = require('./utils/auth');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
-// const routes = require('./routes');
 
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
+  // context: authMiddleware,
 });
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 };
 
-// app.use(routes);
-
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
+
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  // });
+
+
+
+  // app.use(routes);
+
+
 
   db.once('open', () => {
     app.listen(PORT, () => {
